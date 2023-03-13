d0324bafdb03ea519a7db37e8387991f9696cf1a
GuilhermeC0sta <98281473+GuilhermeC0sta@users.noreply.github.com>
2023-03-12 23:35:41 -0300

    Pesquisa por nome

--- myapp/website/admin.py
+++ myapp/website/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 from myapp.models import Alunos, Atividadealuno, Atividades, Disciplinaaluno, Disciplinas, Frequencia, Frequenciaaluno, Planoaula, Professores
 
-admin.site.register(Alunos)
+#admin.site.register(Alunos)
 admin.site.register(Atividadealuno)
 admin.site.register(Atividades)
 admin.site.register(Disciplinaaluno)
@@ -14,3 +14,8 @@ admin.site.register(Frequenciaaluno)
 admin.site.register(Planoaula)
 admin.site.register(Professores)
 
+
+class AlunosAdmin(admin.ModelAdmin):
+    search_fields = ['nome']
+
+admin.site.register(Alunos, AlunosAdmin)
